6900869cf136e3030c00be5ead118c2ca51d5f36
Shree Sharma <sharmashree750@gmail.com>
2026-03-21 10:12:19 +0530

    Time: 3 ms (44.91%), Space: 8.7 MB (37.3%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int reverse(int x){
+        int rev = 0, rem = 0;
+        while(x > 0){
+            rem = x % 10;
+            if(rev > INT_MAX/10 || rev < INT_MIN/10){
+                return 0;
+            }
+            rev = rev*10 + rem;
+            x /= 10;
+        }
+        return rev;
+    }
+    bool isPalindrome(int x) {
+        if(x < 0){
+            return false;
+        }
+        int revNum = reverse(x);
+        if(x == revNum){
+            return true;
+        }
+        return false;
+    }
+};
